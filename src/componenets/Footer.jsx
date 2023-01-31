b645047f6981df4150f037177192fd09af8bf398
Coleman Gorham <cmgorham91@gmail.com>
2023-01-31 11:13:23 -0600

    commit

---
+++ src/componenets/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from '../img/Red.png'
+
+const Footer = () => {
+    return (
+        <footer>
+            <img src={Logo} alt='' />
+            <span>Made by Coleman with <b>React.js</b>.
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
